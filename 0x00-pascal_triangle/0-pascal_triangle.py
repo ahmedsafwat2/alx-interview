12230571b0f9d90d566c096bdc1c17e331c16f4f
Your Name <you@example.com>
2024-08-05 12:11:50 +0000

    alx-interview pascal_triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,14 +10,14 @@ def pascal_triangle(n):
     representing the Pascal Triangle of n
     returns empty list if n <= 0
     """
-    k = []
-    if n <= 0:
-        return k
-    k = [[1]]
-    for row in range(1, n):
-        temp=[1]
-        for col in range(row - 1):
-            temp.append(k[row - 1][col] + k[row - 1][col + 1])
-        temp.append(1)
-        k.append(temp)
-    return k
+	k = []
+	if n <= 0:
+		return k
+	k = [[1]]
+	for row in range(1, n):
+		temp=[1]
+		for col in range(row - 1):
+			temp.append(k[row - 1][col] + k[row - 1][col + 1])
+		temp.append(1)
+		k.append(temp)
+	return k
